c7605e3ccc9e4a4f8ca509686b33b26666ee3b1f
Jesse Hutchison <j.w.hutchison@live.com>
2024-07-04 10:48:27 -0400

    Work on the gameplay scene
    Added the Cinemachine and Input system packages from Unity with the intent to have a CM camera follow the player using the new input system so people can play with mouse & keyboard or gamepads. None of it work currently but its stubbed out. Also created some programmer art assets to use as a template for starting to design. NOT FINAL ART.

---
+++ Assets/GamePlay/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D controller;
+
+    public void Move()
+    {
+        // find and hookup new input to get values to send into the brackeys 2d controller's move method.
+        //controller.Move();
+    }
+}
