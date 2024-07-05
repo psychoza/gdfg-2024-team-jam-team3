ade4274a4f65bfe4c99c52599b670447e8c1ea6b
Jesse Hutchison <j.w.hutchison@live.com>
2024-07-05 19:47:47 -0400

    LeanTween + More programmer art
    A first book power up is in the scene. This one is intended to be the Sight Beyond Sight with the eyeball cover. Collision is not working / no actual power up... yet

---
+++ Assets/GamePlay/Scripts/PowerUp.cs
@@ -0,0 +1,35 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace GamePlay.Scripts
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class PowerUp : MonoBehaviour
+    {
+        [SerializeField] private Collider2D goCollider;
+        
+        [Header("LeanTween Animation Values")]
+        [SerializeField] private float speed = 0.75f;
+        [SerializeField] private float overshoot = 0.75f;
+        [SerializeField] [InspectorLabel("Y Offset")] private float yOffset = 0.125f;
+        
+        private void Start()
+        {
+            LeanTween.moveY(gameObject,transform.position.y + yOffset, speed)
+                .setEase(LeanTweenType.easeInBack)
+                .setOvershoot(overshoot)
+                .setLoopPingPong();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("triggered");
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            Debug.Log("collided");
+            Destroy(gameObject);
+        }
+    }
+}
