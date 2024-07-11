1e131e25648a8252d163b2fad15deae62f8c1019
Jesse Hutchison <j.w.hutchison@live.com>
2024-07-10 20:35:34 -0400

    Enemies take damage

--- Assets/GamePlay/Scripts/Crayonite.cs
+++ Assets/GamePlay/Scripts/Crayonite.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
 // This class defines the behavior and attributes specific to the Crayonite enemy.
 // It inherits from the EnemyStats class to include basic stats like health, damage, and speed.
@@ -24,5 +25,15 @@ public class Crayonite : EnemyStats
     void Update()
     {
         movement.SetSpeed(moveSpeed);
+    }
+
+    protected override void Collide(Collision2D other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+
+        Debug.Log($"{gameObject.name} says 'Ouch, I got jumped on'");
+        base.Collide(other);
+        Destroy(gameObject);
+
     }
 }
--- Assets/GamePlay/Scripts/EnemyStats.cs
+++ Assets/GamePlay/Scripts/EnemyStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,13 @@ using UnityEngine;
 // It includes health points (HP), damage, and movement speed.
 // provides a template for implementing specific enemy statistics.
 
+[RequireComponent(typeof(Collider2D))]
 public abstract class EnemyStats : MonoBehaviour
 {
     // all info about the enemy
     [SerializeField] protected int maxHP = 100;
+    [SerializeField] protected Collider2D bodyCollider;
+    [SerializeField] protected Collider2D headCollider;
 
     [SerializeField] protected int moveSpeed = 10;
     protected int currentHP;
@@ -18,8 +22,31 @@ public abstract class EnemyStats : MonoBehaviour
     protected virtual void Start() 
     {
         currentHP = maxHP; // Set current HP to max HP
+        if (bodyCollider == null || headCollider == null)
+        {
+            Debug.LogError($"Enemy {gameObject.name} missing collider information!");
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.otherCollider == bodyCollider)
+        {
+            Debug.Log($"{gameObject.name} hit {other.gameObject.name}");
+        }
+        
+        if (other.otherCollider == headCollider)
+        {
+            Debug.Log($"{other.gameObject.name} jumped on {gameObject.name}");
+            Collide(other);
+        }
+    }
+
+    protected virtual void Collide(Collision2D other)
+    {
+        
+    }
+    
     // Other methods to manage enemy stats and behavior
     // For example: methods to take damage, die, attack, etc.
     // ...
