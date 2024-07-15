08b13de5810c8e63d43c62cfc89ec1a48fd72ae8
AMKoch82 <109561224+AMKoch82@users.noreply.github.com>
2024-07-14 21:23:02 -0600

    Added art and made the 2d settings for the game view

---
+++ Assets/GamePlay/Scripts/Paralax Background.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class ParalaxBackground : MonoBehaviour
+{ 
+private float _startingPos,
+            _lengthOfSprite;
+public float AmountOfParallax; 
+public Camera MainCamera;
+
+
+
+private void Start()
+{
+    _startingPos = transform.position.x;
+
+    _lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+}
+
+
+
+private void Update()
+{
+    Vector3 Position = MainCamera.transform.position;
+    float Temp = Position.x * (1 - AmountOfParallax);
+    float Distance = Position.x * AmountOfParallax;
+    Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
+    transform.position = NewPosition;
+
+    if (Temp > _startingPos + (_lengthOfSprite / 2))
+    {
+        _startingPos += _lengthOfSprite;
+    }
+    else if (Temp < _startingPos - (_lengthOfSprite / 2))
+    {
+        _startingPos -= _lengthOfSprite;
+    }
+}
+}
